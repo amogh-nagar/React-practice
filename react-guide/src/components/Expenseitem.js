664fd5fb6289efef80614f85b8937356a179c42f
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2021-06-23 12:51:13 +0530

    Added react-guide

---
+++ react-guide/src/components/Expenseitem.js
@@ -0,0 +1,16 @@
+import "./Expenseitem.css";
+
+function Expenseitem(props) {
+   
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Expenseitem;
